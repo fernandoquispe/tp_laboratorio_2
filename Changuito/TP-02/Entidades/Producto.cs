ef8ed3d2eee22e7aca79a699526be6050fe04972
fernandoquispe <fernando.utn@hotmail.com>
2017-05-09 17:28:26 -0300

    Se subre Trabajo Practico 2 - Changuito

---
+++ Changuito/TP-02/Entidades/Producto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    /// <summary>
+    /// La clase Producto será abstracta, evitando que se instancien elementos de este tipo.
+    /// </summary>
+    public abstract class Producto
+    {
+        public enum EMarca { Serenisima, Campagnola, Arcor, Ilolay, Sancor, Pepsico }
+        
+        EMarca _marca;
+        string _codigoDeBarras;
+        ConsoleColor _colorPrimarioEmpaque;
+        
+        public string codigoDeBarras
+        { get { return this._codigoDeBarras;} }
+        
+        public Producto(EMarca marca, string codigoDeBarras, ConsoleColor colorPrimarioEmpaque)
+        {
+            this._codigoDeBarras = codigoDeBarras;
+            this._marca = marca;
+            this._colorPrimarioEmpaque = colorPrimarioEmpaque;
+        }
+
+        /// <summary>
+        /// ReadOnly: Retornará la cantidad de ruedas del vehículo
+        /// </summary>
+        public virtual short CantidadCalorias 
+        { get; set; }
+             
+        /// <summary>
+        /// Publica todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        //public void Mostrar()
+        public virtual string Mostrar()
+        {
+            return this;
+        }
+
+        public static implicit operator string(Producto p)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CODIGO DE BARRAS: " +p._codigoDeBarras);
+            sb.AppendLine("MARCA          : " +p._marca.ToString());
+            sb.AppendLine("COLOR EMPAQUE  : " +p._colorPrimarioEmpaque.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        
+        /// <summary>
+        /// Dos productos son iguales si comparten el mismo código de barras
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Producto v1, Producto v2)
+        {
+            return v1._codigoDeBarras == v2._codigoDeBarras;
+        }
+
+        /// <summary>
+        /// Dos productos son distintos si su código de barras es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Producto v1, Producto v2)
+        {
+           return !(v1._codigoDeBarras == v2._codigoDeBarras);
+        }
+    }
+}
