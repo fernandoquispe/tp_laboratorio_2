e4c0988450062c94a9d10bba2b994f5da7b23d30
fernandoquispe <fernando.utn@hotmail.com>
2017-09-14 10:12:48 -0300

    Se sube TP01

---
+++ TP01/Calculadora/Calculadora.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    class Calculadora
+    {
+        /// <summary>
+        /// Realiza la operacion indicada entre dos numeros, retorna el resultado de dicha operacion.
+        /// Si no se puede operar, division por cero, retornara cero.
+        /// </summary>
+        /// <param name="numero1"></param>
+        /// <param name="numero2"></param>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        public static double operar(Numero numero1, Numero numero2, string operador)
+        {
+            double result = 0;
+            double n1;
+            double n2;
+
+            operador = validarOperador(operador);
+            n1 = numero1.getNumero();
+            n2 = numero2.getNumero();
+
+            switch (operador)
+            {
+                case "+":
+                    result = n1 + n2;
+                    break;
+
+                case "-":
+                    result = n1 - n2;
+                    break;
+
+                case "*":
+                    result = n1 * n2;
+                    break;
+
+                case "/":
+                    if (n2 != 0)
+                    {
+                        result = n1 / n2;
+                    }
+                    break;
+            }
+            return result;
+     }
+
+        /// <summary>
+        /// Valida el operador si es alguno de los validos, caso contrario toma por defecto la suma.
+        /// </summary>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        public static string validarOperador(string operador)
+        {
+            string resultado = operador;
+
+            if (operador != "+" && operador != "-" && operador != "*" && operador != "/")
+            {
+                resultado = "+";
+            }
+            return resultado;
+        }
+    }
+}
---
+++ TP01/Calculadora/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Boton CC, el cual se encarga de limpiar la pantalla de la calculadora.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Clear();
+            txtNumero2.Clear();
+            lblResultado.Text = string.Empty;
+            cmbOperacion.SelectedItem = null;
+            cmbOperacion.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// Boton igual que realiza el calculo mediante el metodo "Operar" de la clase Calculadora.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            Numero numero1 = new Numero(txtNumero1.Text);
+            Numero numero2 = new Numero(txtNumero2.Text);
+            double resultado = 0;
+            string operador = string.Empty;
+
+            if (cmbOperacion.SelectedItem != null)
+            {
+                operador = cmbOperacion.SelectedItem.ToString();
+            }
+
+            resultado = Calculadora.operar(numero1, numero2, operador);
+
+            lblResultado.Text = resultado.ToString();
+        }          
+    }
+}
